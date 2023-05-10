e7e62df5d799cb3764d31a40737436b940cdb6c3
Quang Le <leanhquang2803@gmail.com>
2023-05-10 14:01:34 -0500

    Update convert_wav and perform_cnn

--- convert_wav.py
+++ convert_wav.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from collections import defaultdict
 from pydub import AudioSegment
 
-df = pd.read_csv('./dataset_clean_1683243179.422074.csv', dtype={'track_genre': str})
+df = pd.read_csv('./data_200_genre.csv', dtype={'track_genre': str})
 df_purl = df[df[['preview_url']].notnull().all(1)]
 
 os.mkdir('mp3_files')
@@ -28,4 +28,6 @@ for idx, row in df_purl.iterrows():
                                  
     sound = AudioSegment.from_mp3(src)
     sound.export(dst, format="wav")
+
+    
 
